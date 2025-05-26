2d2c48c6fe37b92f792f45f2627db981e4bfe6a6
Miguel Cardoso <migueltranslatorbr@gmail.com>
2025-05-26 17:19:47 -0300

    commitando

---
+++ src/main/java/ulbra/br/APIap2/ApIap2Application.java
@@ -0,0 +1,13 @@
+package ulbra.br.APIap2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIap2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIap2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ulbra/br/APIap2/ApIap2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ulbra.br.APIap2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIap2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
